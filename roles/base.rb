43bb82401936bcb9859677aaf94c559f8e8e5c3a
Hector Castro <hectcastro@gmail.com>
2014-01-06 09:37:01 -0500

    Removed sysctl specific settings because they are handled by the Riak cookbook since version 2.4.2.

--- roles/base.rb
+++ roles/base.rb
@@ -2,33 +2,11 @@ name "base"
 description "Base role."
 run_list(
   "recipe[yum]",
-  "recipe[sysctl]",
   "recipe[ntp]",
   "recipe[java]",
   "recipe[openssh]"
 )
 default_attributes(
-  "sysctl" => {
-    "params" => {
-      "vm" => {
-        "swappiness" => 0
-      },
-      "net" => {
-        "core" => {
-          "somaxconn" => 40000
-        },
-        "ipv4" => {
-          "tcp_max_syn_backlog" => 40000,
-          "tcp_timestamps" => 0,
-          "tcp_sack" => 1,
-          "tcp_window_scaling" => 1,
-          "tcp_fin_timeout" => 15,
-          "tcp_keepalive_intvl" => 30,
-          "tcp_tw_reuse" => 1
-        }
-      }
-    }
-  },
   "java" => {
     "install_flavor" => "oracle",
     "jdk_version" => 7,
